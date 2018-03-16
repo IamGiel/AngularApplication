4cc3f93e2bfe4055318df8b6e27ec09e4c6c1905
Server <november14giel@gmail.com>
2018-03-16 11:55:18 -0400

    heroku deploy1

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+// const mongoose = require("mongoose");
+const path = require("path");
+// const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// Serve up static assets
+app.use(express.static(path.join(__dirname, "/dist")));
+
+// Angular path location strategy
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname + "/dist/index.html"));
+})
+// Set up promises with mongoose
+// mongoose.Promise = global.Promise;
+// Connect to the Mongo DB
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
+//   {
+//     // useMongoClient: true
+//   }
+// );
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
