d8654246371cca5def426db9a05d6d002e8c30cc
Server <november14giel@gmail.com>
2018-06-22 15:50:37 -0400

    change to lifezone church app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,6 @@ import { Routes, RouterModule } from "@angular/router";
 // adding routes
 // adding the components
 import { HomeComponent } from './home/home.component'
-import { AboutComponent } from "./about/about.component";
-import { HeroesComponent } from "./heroes/heroes.component";
-import { TraversyComponent } from "./traversy/traversy.component";
 
 
 //we use routes array and have a json of paths
@@ -14,19 +11,19 @@ const routes: Routes = [
   {
     path: "",
     component: HomeComponent
-  },
-  {
-    path: "about",
-    component: AboutComponent
-  },
-  {
-    path: "heroes",
-    component: HeroesComponent
-  },
-  {
-    path: "traversy",
-    component: TraversyComponent
   }
+  // {
+  //   path: "about",
+  //   component: AboutComponent
+  // },
+  // {
+  //   path: "heroes",
+  //   component: HeroesComponent
+  // },
+  // {
+  //   path: "traversy",
+  //   component: TraversyComponent
+  // }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,19 @@ import { AppRoutingModule } from "./app-routing.module";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations"; // npm install @angular/animations@latest --save
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
-import { AboutComponent } from "./about/about.component";
+
 import { DataService } from "./data.service";
-import { HeroesComponent } from './heroes/heroes.component';
-import { heroService } from "./heroes/heroes.service";
-import { TraversyComponent } from './traversy/traversy.component';
+
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, AboutComponent, HeroesComponent, TraversyComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule, //double binding
     BrowserAnimationsModule
   ],
-  providers: [DataService, heroService],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
