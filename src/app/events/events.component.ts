ef62ed3b22d9476c5386f78616793b20a68da311
Server <november14giel@gmail.com>
2018-06-23 13:13:06 -0400

    work on carousel

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+
+declare var M: any; // 👈🏼to use `M` in Materialize
+
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.scss']
 })
+
+
 export class EventsComponent implements OnInit {
+
+  //variables
 
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
