5c9d19f7148eafc3b8490feef2ddf4513ceae564
Server <november14giel@gmail.com>
2018-06-30 17:12:18 -0400

    admin form

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { EmailValidator } from "@angular/forms";
 
 @Component({
   // selector: "app-server",
@@ -9,16 +10,8 @@ import { Component, OnInit } from "@angular/core";
 export class ServerComponent {
   //properties here:
   btnDisabled: Boolean = false; // disable button using property binding
-  hideForm: boolean = false;
-  // event headers
-  defaultTitle = "Event Title";
-  photoUrl: any;
-  defaultNote =
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex eveniet quis, accusantium odit a porro doloribus blanditiis vitae repudiandae quo rem alias beatae sed deleniti enim, ad odio cumque et.";
-
-  eventTitle: any;
-  eventPhoto: any;
-  eventNote: any;
+  userEmail: string = "enter@email.com";
+  adminForm: boolean = true;
 
   constructor() {
     setTimeout(() => {
@@ -26,43 +19,11 @@ export class ServerComponent {
     }, 3000);
   }
 
-  //if the user has typed values in the form allow display of event
-  // displayEvent(){
-  //   if()
-  // }
-
   //methods
 
-  onAdminForm() {
-    this.hideForm = !this.hideForm;
+  //hide admin form
+  hideForm(){
+    this.adminForm = !this.adminForm;
   }
 
-  //input
-  onInputName(event: any) {
-    // reference to DOM's $event = which gives access to events data
-    console.log(event);
-    this.defaultTitle = (<HTMLInputElement>event.target).value;
-  }
-  onInputPhoto(event: any) {
-    // reference to DOM's $event = which gives access to events data
-    console.log(event);
-    this.photoUrl = (<HTMLInputElement>event.target).value;
-  }
-  onInputNote(event: any) {
-    // reference to DOM's $event = which gives access to events data
-    console.log(event);
-    this.defaultNote = (<HTMLInputElement>event.target).value;
-  }
-  onMouseOut() {
-    console.log("hello im out");
-    this.hideForm = false;
-  }
-  printEvent(e) {
-    e.preventDefault();
-    console.log("printing event");
-    this.defaultTitle = this.eventTitle;
-    this.photoUrl = this.eventPhoto;
-    this.defaultNote = this.eventNote;
-    this.hideForm = !this.hideForm;
-  }
 }
